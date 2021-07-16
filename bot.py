9677f7732574b406e66a5527f187a9548538ff03
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-07-16 16:08:14 +0200

    Basic ChatBot finished

--- bot.py
+++ bot.py
@@ -9,15 +9,17 @@ Chat bot main code
 # ****************************************
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+import string
 
 CHATBOT = ChatBot('Virtual assistant') # Creating a ChatBot
-CHATBOT.storage.drop()
+CHATBOT.storage.drop() # Eliminates the dialog data
 CONVERSATION = open('knowledge.bc', 'r').readlines()
 TRAINER = ListTrainer(CHATBOT) # This will train the CHATBOT object
 
 TRAINER.train(CONVERSATION) # Training the bot with the CONVERSATION list
-
 while True: # Communication flow
     REQUEST = input('Me: ') # Introduced by the user
     ANSWER = CHATBOT.get_response(REQUEST) # Bot's answer
     print('Virtual assistant: ', ANSWER)
+    if str(ANSWER).startswith("See"): # Finish condition
+        break

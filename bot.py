380eb1c0f9a4d0445ef47eb1d5d2f20f6794be12
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-07-18 11:02:42 +0200

    Classe de colors, knowledge actualitzat, bot avançat

--- bot.py
+++ bot.py
@@ -9,17 +9,23 @@ Chat bot main code
 # ****************************************
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-import string
-
+import urlib.parse
+import colors
+print(colors.color.YELLOW + "********** Loading all data **********" + colors.color.END)
 CHATBOT = ChatBot('Virtual assistant') # Creating a ChatBot
 CHATBOT.storage.drop() # Eliminates the dialog data
 CONVERSATION = open('knowledge.bc', 'r').readlines()
 TRAINER = ListTrainer(CHATBOT) # This will train the CHATBOT object
 
 TRAINER.train(CONVERSATION) # Training the bot with the CONVERSATION list
+print(colors.color.CYAN + "Welcome, I'm Charlie and I'm going to be your virtual assitent today." + colors.color.END)
 while True: # Communication flow
     REQUEST = input('Me: ') # Introduced by the user
     ANSWER = CHATBOT.get_response(REQUEST) # Bot's answer
-    print('Virtual assistant: ', ANSWER)
+    print('Charlie: ', ANSWER)
     if str(ANSWER).startswith("See"): # Finish condition
         break
+    if str(REUQEST) == "DGT":
+        print(colors.color.BLUE + "Acces to the following link: " + colors.color.END)
+        institution = urlib.parse.quote('www.dgt.es/es/')
+        print("https://" + institution)

df75b92672f3af3176a46cddf052e921b9432101
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-07-18 12:59:22 +0200

    S'ha de revisar els errors

--- bot.py
+++ bot.py
@@ -9,8 +9,18 @@ Chat bot main code
 # ****************************************
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-import urlib.parse
+import urllib.parse
+import string
 import colors
+# ******************* Auxiliar methods zone *******************
+
+def print_instructions():
+    file = open('instructions.txt', 'r')
+    content = file.read()
+    print(colors.color.GREEN + content + colors.color.END)
+    file.close()
+
+
 print(colors.color.YELLOW + "********** Loading all data **********" + colors.color.END)
 CHATBOT = ChatBot('Virtual assistant') # Creating a ChatBot
 CHATBOT.storage.drop() # Eliminates the dialog data
@@ -19,13 +29,32 @@ TRAINER = ListTrainer(CHATBOT) # This will train the CHATBOT object
 
 TRAINER.train(CONVERSATION) # Training the bot with the CONVERSATION list
 print(colors.color.CYAN + "Welcome, I'm Charlie and I'm going to be your virtual assitent today." + colors.color.END)
+print_instructions()
 while True: # Communication flow
     REQUEST = input('Me: ') # Introduced by the user
     ANSWER = CHATBOT.get_response(REQUEST) # Bot's answer
     print('Charlie: ', ANSWER)
+
+    if str(REQUEST)== "Traffic ticket" or str(REQUEST)== "traffic ticket":
+        REQUEST = input('Me: ') # Introduced by the user
+        ANSWER = CHATBOT.get_response(REQUEST) # Bot's answer
+        print('Charlie: ', ANSWER)
+        if str(REQUEST) == "presentially" or str(REQUEST) == "Presentially":
+            REQUEST = input('Me: ') # Introduced by the user
+            ANSWER = CHATBOT.get_response(REQUEST) # Bot's answer
+            print('Charlie: ', ANSWER)
+            if str(REQUEST)== "lleida" or str(REQUEST)== "Lleida":
+                print('Charlie: ', ANSWER)
+                print(colors.color.BLUE + "Citizen attention office of Lleida link: " + colors.color.END)
+                lleida_attention = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=12443')
+                print("https://" + lleida_attention)
+                print(colors.color.BLUE + "Territorial traffic servie of Lleida link: " + colors.color.END)
+                lleida_service = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=10293')
+                print("https://" + lleida_service)
+
+
+
+
+    #print('Charlie: ', ANSWER)
     if str(ANSWER).startswith("See"): # Finish condition
         break
-    if str(REUQEST) == "DGT":
-        print(colors.color.BLUE + "Acces to the following link: " + colors.color.END)
-        institution = urlib.parse.quote('sede.dgt.gob.es/es/multas/#')
-        print("https://" + institution)

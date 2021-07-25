218ac695a0559f98b0b269e5171f1fdd6ed31d06
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-07-25 13:16:30 +0200

    Aançant el bot v1.1

--- bot.py
+++ bot.py
@@ -12,6 +12,8 @@ from chatterbot.trainers import ListTrainer
 import urllib.parse
 import string
 import colors
+
+
 # ******************* Auxiliar methods zone *******************
 
 def print_instructions():
@@ -22,39 +24,31 @@ def print_instructions():
 
 
 print(colors.color.YELLOW + "********** Loading all data **********" + colors.color.END)
-CHATBOT = ChatBot('Virtual assistant') # Creating a ChatBot
-CHATBOT.storage.drop() # Eliminates the dialog data
+CHATBOT = ChatBot('Virtual assistant')  # Creating a ChatBot
+CHATBOT.storage.drop()  # Eliminates the dialog data
 CONVERSATION = open('knowledge.bc', 'r').readlines()
-TRAINER = ListTrainer(CHATBOT) # This will train the CHATBOT object
+TRAINER = ListTrainer(CHATBOT)  # This will train the CHATBOT object
 
-TRAINER.train(CONVERSATION) # Training the bot with the CONVERSATION list
-print(colors.color.CYAN + "Welcome, I'm Charlie and I'm going to be your virtual assitent today." + colors.color.END)
+TRAINER.train(CONVERSATION)  # Training the bot with the CONVERSATION list
+print(colors.color.CYAN + "Welcome, I'm Charlie and I'm going to be your virtual assistant today." + colors.color.END)
 print_instructions()
-while True: # Communication flow
-    REQUEST = input('Me: ') # Introduced by the user
-    ANSWER = CHATBOT.get_response(REQUEST) # Bot's answer
+while True:  # Communication flow
+    REQUEST = input('Me: ')  # Introduced by the user
+    ANSWER = CHATBOT.get_response(REQUEST)  # Bot's answer
     print('Charlie: ', ANSWER)
-    if str(REQUEST) == "Traffic ticket":
-        REQUEST = input('Me: ')
-        ANSWER = CHATBOT.get_response(REQUEST)
+    if str(REQUEST) == "Presentially":
+        REQUEST = input('Me: ')  # Introduced by the user
+        ANSWER = CHATBOT.get_response(REQUEST)  # Bot's answer
         print('Charlie: ', ANSWER)
-        if str(ANSWER) == "Yes":
-            REQUEST = input('Me: ')
-            ANSWER = CHATBOT.get_response(REQUEST)
-            print('Charlie: ', ANSWER)
-            if str(REQUEST) == "Presentially":
-                REQUEST = input('Me: ')
-                ANSWER = CHATBOT.get_response(REQUEST)
-                print('Charlie: ', ANSWER)
-                if str(ANSWER) == "Links of Lleida:":
-                    print(colors.color.BLUE + "Citizen attention office of Lleida link: " + colors.color.END)
-                    lleida_attention = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=12443')
-                    print("https://" + lleida_attention)
+        if str(REQUEST) == "Lleida":
+            print('Links of Lleida: ')
+            print(colors.color.BLUE + "Citizen attention office of Lleida link: " + colors.color.END)
+            lleida_attention = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=12443')
+            print("http://", lleida_attention)
+            print(colors.color.BLUE + "Territorial traffic service of Lleida link: " + colors.color.END)
+            lleida_service = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=10293')
+            print("http://", lleida_service)
 
-
-
-
-    #print('Charlie: ', ANSWER)
-    if str(ANSWER).startswith("See"): # Finish condition
+    # print('Charlie: ', ANSWER)
+    if str(ANSWER).startswith("See"):  # Finish condition
         break
-        str(ANSWER) == "See you the next time!"

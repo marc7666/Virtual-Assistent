07ff266f6194b98e6fa9a00358c906bc04bf8cd8
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-07-27 12:57:52 +0200

    Pylint passat

--- bot.py
+++ bot.py
@@ -1,22 +1,24 @@
-'''
+"""
 Chat bot main code
-'''
+"""
 # ****************************************
 # Authorship:
 # Jordi Planes Cid
 # Marc Cervera Rosell
 # Polytecnich school - University of Lleida
 # ****************************************
+import urllib.parse
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
-import urllib.parse
-import string
 import colors
 
 
 # ******************* Auxiliar methods zone *******************
 
 def print_instructions():
+    """
+    This method prints the instructions file
+    """
     file = open('instructions.txt', 'r')
     content = file.read()
     print(colors.color.GREEN + content + colors.color.END)
@@ -24,6 +26,9 @@ def print_instructions():
 
 
 def print_phone_information():
+    """
+    This method prints the 012 phone information.
+    """
     file = open('012_information.txt', 'r')
     content = file.read()
     print(colors.color.GREEN + content + colors.color.END)
@@ -37,7 +42,8 @@ CONVERSATION = open('knowledge.bc', 'r').readlines()
 TRAINER = ListTrainer(CHATBOT)  # This will train the CHATBOT object
 
 TRAINER.train(CONVERSATION)  # Training the bot with the CONVERSATION list
-print(colors.color.CYAN + "Welcome, I'm Charlie and I'm going to be your virtual assistant today." + colors.color.END)
+print(colors.color.CYAN + "Welcome, I'm Charlie and I'm going to be your virtual assistant today."
+      + colors.color.END)
 print_instructions()
 while True:  # Communication flow
     REQUEST = input('Me: ')  # Introduced by the user
@@ -48,57 +54,81 @@ while True:  # Communication flow
         ANSWER = CHATBOT.get_response(REQUEST)  # Bot's answer
         print('Charlie: ', ANSWER)
         if str(REQUEST) == "Lleida":
-            print(colors.color.BLUE + "Citizen attention office of Lleida link: " + colors.color.END)
-            lleida_attention = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=12443')
-            print('http://' + lleida_attention)
-            print(colors.color.BLUE + "Territorial traffic service of Lleida link: " + colors.color.END)
-            lleida_service = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=10293')
-            print('http://' + lleida_service)
+            print(colors.color.BLUE + "Citizen attention office of Lleida link: "
+                  + colors.color.END)
+            LLEIDA_ATTENTION = urllib.parse.quote(
+                'sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=12443')
+            print('http://' + LLEIDA_ATTENTION)
+            print(colors.color.BLUE + "Territorial traffic service of Lleida link: "
+                  + colors.color.END)
+            LLEIDA_SERVICE = urllib.parse.quote(
+                'sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=10293')
+            print('http://' + LLEIDA_SERVICE)
         elif str(REQUEST) == "Barcelona":
-            print(colors.color.BLUE + "Citizen attention office of Barcelona link: " + colors.color.END)
-            barcelona_attention = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=6622')
-            print('http://' + barcelona_attention)
-            print(colors.color.BLUE + "Territorial traffic service of Barcelona link: " + colors.color.END)
-            barcelona_service = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=10995')
-            print('http://' + barcelona_service)
+            print(colors.color.BLUE + "Citizen attention office of Barcelona link: "
+                  + colors.color.END)
+            BARCELONA_ATTENTION = urllib.parse.quote(
+                'sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=6622')
+            print('http://' + BARCELONA_ATTENTION)
+            print(colors.color.BLUE + "Territorial traffic service of Barcelona link: "
+                  + colors.color.END)
+            BARCELONA_SERVICE = urllib.parse.quote(
+                'sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=10995')
+            print('http://' + BARCELONA_SERVICE)
         elif str(REQUEST) == "Tarragona":
-            print(colors.color.BLUE + "Citizen attention office of Tarragona link: " + colors.color.END)
-            tarragona_attention = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=12444')
-            print('http://' + tarragona_attention)
-            print(colors.color.BLUE + "Territorial traffic service of Tarragona link: " + colors.color.END)
-            tarragona_service = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=10996')
-            print('http://' + tarragona_service)
+            print(colors.color.BLUE + "Citizen attention office of Tarragona link: "
+                  + colors.color.END)
+            TARRAGONA_ATTENTION = urllib.parse.quote(
+                'sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=12444')
+            print('http://' + TARRAGONA_ATTENTION)
+            print(colors.color.BLUE + "Territorial traffic service of Tarragona link: "
+                  + colors.color.END)
+            TARRAGONA_SERVICE = urllib.parse.quote(
+                'sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=10996')
+            print('http://' + TARRAGONA_SERVICE)
         elif str(REQUEST) == "Girona":
-            print(colors.color.BLUE + "Citizen attention office of Girona link: " + colors.color.END)
-            girona_attention = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=17461')
-            print('http://' + girona_attention)
-            print(colors.color.BLUE + "Territorial traffic service of Girona link: " + colors.color.END)
-            girona_service = urllib.parse.quote('sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=9715')
-            print('http://' + girona_service)
+            print(colors.color.BLUE + "Citizen attention office of Girona link: "
+                  + colors.color.END)
+            GIRONA_ATTENTION = urllib.parse.quote(
+                'sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=17461')
+            print('http://' + GIRONA_ATTENTION)
+            print(colors.color.BLUE + "Territorial traffic service of Girona link: "
+                  + colors.color.END)
+            GIRONA_SERVICE = urllib.parse.quote(
+                'sac.gencat.cat/sacgencat/AppJava/organisme_fitxa.jsp?codi=9715')
+            print('http://' + GIRONA_SERVICE)
     if str(REQUEST) == "Phone":
         print(colors.color.BLUE + "Check the 012 opening hours:" + colors.color.END)
-        opening_hours = urllib.parse.quote('web.gencat.cat/ca/contacte/012/')
-        print('http://' + opening_hours)
+        OPENING_HOURS = urllib.parse.quote('web.gencat.cat/ca/contacte/012/')
+        print('http://' + OPENING_HOURS)
         print_phone_information()
     if str(REQUEST) == "Via internet":
         print(colors.color.BLUE + "Make the payment with digital certificate" + colors.color.END)
-        pay_and_certificate = urllib.parse.quote(
-            'identitats.aoc.cat/o/oauth2/auth?response_type=code&client_id=tramits.transit.cat&redirect_uri=https'
-            '://multestransit.gencat.cat/sctPagaments/AppJava/loginIdCat&scope=autenticacio_usuari&access_type=online'
+        PAY_AND_CERTIFICATE = urllib.parse.quote(
+            'identitats.aoc.cat/o/oauth2/auth?response_type=code&client_id=tramits.'
+            'transit.cat&redirect_uri=https'
+            '://multestransit.gencat.cat/sctPagaments/AppJava/loginIdCat&scope='
+            'autenticacio_usuari&access_type=online'
             '&approval_pompt=false&state=ca_ES')
-        print('https://' + pay_and_certificate)
-        print(colors.color.BLUE + "Make the payment without digital certificate" + colors.color.END)
-        pay_without_certificate = urllib.parse.quote(
-            'multestransit.gencat.cat/sctPagaments/AppJava/views/expedients/cerca.xhtml?set-locale=ca_ES')
-        print('https://' + pay_without_certificate)
+        print('https://' + PAY_AND_CERTIFICATE)
+        print(colors.color.BLUE + "Make the payment without digital certificate"
+              + colors.color.END)
+        PAY_WITHOUT_CERTIFICATE = urllib.parse.quote(
+            'multestransit.gencat.cat/sctPagaments/AppJava/views/expedients/cerca.'
+            'xhtml?set-locale=ca_ES')
+        print('https://' + PAY_WITHOUT_CERTIFICATE)
     if str(REQUEST) == "No":
-        print(colors.color.BLUE + "Link of the 'Dirección general de tráfico' web to pay a fine" + colors.color.END)
-        dgt = urllib.parse.quote('sede.dgt.gob.es/es/multas/paga-tu-multa/')
-        print('https://' + dgt)
+        print(colors.color.BLUE + "Link of the 'Dirección general de tráfico' web to pay a fine"
+              + colors.color.END)
+        DGT = urllib.parse.quote('sede.DGT.gob.es/es/multas/paga-tu-multa/')
+        print('https://' + DGT)
     if str(REQUEST) == "I want to present an allegation to a fine":
         print(
-            colors.color.BLUE + "Link of the 'Dirección general de tráfico' web to present an allegation" + colors.color.END)
-        allegation_dgt = urllib.parse.quote('sede.dgt.gob.es/es/multas/presenta-una-alegacion-o-recurso/')
-        print('https://' + allegation_dgt)
+            colors.color.BLUE + "Link of the 'Dirección general de tráfico' web "
+                                "to present an allegation"
+            + colors.color.END)
+        ALLEGATION_DGT = urllib.parse.quote(
+            'sede.DGT.gob.es/es/multas/presenta-una-alegacion-o-recurso/')
+        print('https://' + ALLEGATION_DGT)
     if str(ANSWER) == "See you the next time!":
         break
